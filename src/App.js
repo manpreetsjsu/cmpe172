41e7be3ce374fd8b9e6ec00467f9dbe0c61407a7
manpreetsjsu <manpreetsingh_er@yahoo.com>
2020-04-26 13:51:09 -0700

    Authentication Added with AWS Amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import { Auth, Hub } from 'aws-amplify';
+import Button from '@material-ui/core/Button';
+import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Code Reference:-  https://docs.amplify.aws/lib/auth/social/q/platform/js#setup-frontend
+class App extends Component {
+  state = { user: null, customState: null };
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        console.log(user);
+        this.setState({ user });
+      })
+      .catch(() => console.log("Not signed in"));
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div style={{
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center"
+      }}>
+        {/* // if user is not logged in */}
+          {!this.state.user && 
+          <>
+          <img src={logo} height="100"></img>
+          <Button color="primary" variant="contained" onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign In With Google</Button>
+          </>
+          }
+          {/* if user is logged in */}
+          {
+            <div>Welcome to CMPE 172 Project!</div>
+          }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import react, { Component } from "react";
+
+
+class HomeScreen extends Component{
+
+
+    render(){
+        return(
+            <div>Welcome to CMPE172 Project!</div>
+        )
+    }
+}
