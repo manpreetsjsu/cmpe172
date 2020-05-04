36b9d0c02f0a9c7bae40386ec29bb8955ba3f6df
Sar Padda <sarpadda@Sars-MacBook-Pro.local>
2020-05-03 21:29:51 -0700

    Deleting the API keys

--- src/environment.js
+++ src/environment.js
@@ -1,10 +1,10 @@
 
 // file to keep all urls and keys at one place so it is easy to make changes
 export const environement_read_values =  {
-    API_GATEWAY_KEY: "v0SIoOuwwO1vrCv22YW6t8ruCALATcRZ4uPSe4T6",
-    API_QUERY_WITH_USERNAME_URL : "https://oulut37ra7.execute-api.us-east-1.amazonaws.com/test"
+    API_GATEWAY_KEY: "KEY",
+    API_QUERY_WITH_USERNAME_URL : "URL"
 }
 export const environement_write_values =  {
-    API_GATEWAY_KEY: "v0SIoOuwwO1vrCv22YW6t8ruCALATcRZ4uPSe4T6",
-    API_QUERY_WITH_USERNAME_URL : "https://bvotcd0731.execute-api.us-east-1.amazonaws.com/test"
+    API_GATEWAY_KEY: "Key",
+    API_QUERY_WITH_USERNAME_URL : "URL"
 }
